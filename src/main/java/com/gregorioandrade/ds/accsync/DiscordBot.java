76e928b2feaa737afa14caebfa6427acf36d68a4
Gregorio Andrade <gregorioaar@outlook.es>
2021-08-31 23:32:38 -0300

    Added dummy implementation of verification command

--- src/main/java/com/gregorioandrade/ds/accsync/DiscordBot.java
+++ src/main/java/com/gregorioandrade/ds/accsync/DiscordBot.java
@@ -3,6 +3,8 @@ package com.gregorioandrade.ds.accsync;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.User;
 
 public class DiscordBot {
 
@@ -14,6 +16,20 @@ public class DiscordBot {
 
         client.getEventDispatcher().on(ReadyEvent.class)
                 .subscribe(event -> System.out.println("Now I'm the man on the inside looking out."));
+
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+                .filter(event -> event.getGuildId().isPresent()) // So it can only be used in guild channels
+                .map(MessageCreateEvent::getMessage)
+                .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
+                .filter(message -> message.getContent().equalsIgnoreCase("!verificar"))
+                .filterWhen(message -> message.getChannel().map(channel -> {
+                    channel.createMessage("Código enviado, revisa tus mensajes privados.").subscribe();
+                    return true;
+                }))
+                .map(message -> message.getAuthor().get())
+                .flatMap(User::getPrivateChannel)
+                .flatMap(channel -> channel.createMessage("Tu código es: "+Utils.generateRandomSixDigitString()))
+                .subscribe();
 
         client.onDisconnect().block();
     }
